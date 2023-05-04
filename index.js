36005d01a959038d7683707ebca603dec733c2af
asafridehossain171@gmail.com <asafridehossain171@gmail.com>
2023-05-04 20:41:27 +0600

    allchef data succesfully serving to client

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const app = express();
 const cors = require("cors");
 const port = 3000;
 
+// all chef data
+const allchefjson = require("./jsonData/chefInfo.json");
 app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+app.get("/allchef", (req, res) => {
+  res.send(allchefjson);
 });
 
 app.listen(port, () => {
